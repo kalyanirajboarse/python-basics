9c7241e9d44e184ec54aa43b1275c6164ee3ff84
kalyani borse <borsekalyani28@gmail.com>
2026-04-09 12:36:50 +0530

    understanding python interpretetion line by line and debugging improvements

--- basics-02.py
+++ basics-02.py
@@ -1,7 +1,7 @@
 # file_1= "siya02"
 
 # file_2= "nil001"
-print
+
 # file_3= file_1 + file_2
 
 
@@ -28,11 +28,21 @@ c=a+b
 d=a*b
 
 
-print(c)
+print("c ", c)
 
-print(d)
+print("d ", d)
 
 # learnig dictionary and tuples
 # creating sample data for dictionary and tuples
 my_dict= {"name": "siya", "age": 25, "city": "delhi"}
 my_tuple= ("apple", "banana", "cherry")
+
+
+
+cat_1= 12
+cat_2= "15"
+cat_3= 2.12
+
+print("cat_1 ", cat_1)
+print("cat_2 ", cat_2)
+print("cat_3 ", cat_3)
